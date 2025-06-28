2be1a043ac933e81ca65204199631af827038008
JadweR <jadwer@msn.com>
2025-06-28 17:14:10 -0600

    Structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-    import { Button } from 'react-bootstrap';
+@import '~bootstrap/scss/bootstrap';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
-         <Button variant="primary">Click me</Button>
       </body>
     </html>
   );
